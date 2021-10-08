4b81ce5feb7375540155df496b5bdbb024fede0e
= <=>
2021-10-07 21:11:24 -0300

    criado service.js e iniciado separação de funções

--- shortest-path/backend/server.js
+++ shortest-path/backend/server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express(), bodyParser = require("body-parser");
 const port = 3080;
 const pg = require('pg');
-const client = new pg.Client("postgres://postgres:admin@localhost:5432/shortest-path");
+const client = new pg.Client("postgres://postgres:postgres@localhost:5432/shortest-path");
 const service = require('./service');
-
 
 app.use(bodyParser.json());
 
@@ -12,7 +11,7 @@ app.post('/path/obter-caminho', (req, res) => {
   if (!Object.is(req.body, null)) {
     client.connect(function (err) {
       if (err) throw err;
-      client.query("SELECT * FROM path WHERE path.origem LIKE " + "'" + origem + "'", function (err, result){
+      client.query("SELECT * FROM path WHERE path.origem LIKE " + "'" + req.body.origem + "'", function (err, result){
         if (err) throw err;
         res.json(result);
       });
@@ -21,15 +20,11 @@ app.post('/path/obter-caminho', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  client.connect(function(err) {
-    if (err) throw err;
-    client.query("select * from path", function (err, result) {
-      if (err) throw err;
-      res.json(result.rows);
-    });
+  service.buscarTodosCaminhos(function (result) {
+    res.json(result);
   });
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on the port::${port}`);
+  console.log(`Server on na porta:${port}`);
 });
--- shortest-path/backend/service.js
+++ shortest-path/backend/service.js
@@ -1,7 +1,18 @@
-
+const pg = require('pg');
+const client = new pg.Client("postgres://postgres:postgres@localhost:5432/shortest-path");
 
 module.exports = {
 
+  buscarTodosCaminhos:function (result) {
+    client.connect(function (err) {
+      if (err) throw err;
+      client.query("select * from path", function (err, res){
+        if (err) throw err;
+        return result(res.rows);
+      });
+      client.end;
+    });
+  }
 
 
 
