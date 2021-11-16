e55a91adca62c1f726f74cae1171a9dc5952a625
Bruno Staroski <bruno.oks1@gmail.com>
2021-11-16 19:48:36 -0300

    funcionando
    - ajustado logica para não percorrer lugares visitados
    - ajustado bug conexão bd não fechando e travando a aplicação

--- shortest-path/backend/service.js
+++ shortest-path/backend/service.js
@@ -2,43 +2,21 @@ const pg = require('pg');
 const pool = new pg.Pool({user: 'postgres',host: 'localhost',database: 'shortest-path',password: 'docker',port: 5432});
 const Path = require('./dto/pathDTO');
 const Resultado = require('./dto/resultadoDTO');
-const isNull = require("util");
 
 module.exports = {
-
-  buscarTodosCaminhos: function (result) {
-    pool.connect(function (err, client, done) {
-      if (err) throw err
-      client.query("select * from path", function (err, res) {
-        done();
-        if (err) throw err
-        if (!Object.is(res.rows, null)) {
-          let path = new Array();
-          res.rows.forEach(temp => {
-            path.push(new Path(temp.origem, temp.distancia, temp.destino));
-          })
-          return result(path);
-        }
-      });
-    });
-  },
 
   buscarConexoes: async function (origem, visitados) {
     try {
       const client = await pool.connect();
-      let result = null;
-      if (visitados == null) {
-        let result = await client.query("select * from path where path.origem like" + "'" + origem + "'");
-      } else {
-        let result = await client.query("select * from path where path.origem like" + "'" + origem + "' and path.destino not in (?)", visitados);
-      }
+      let result = await client.query("select * from path where path.origem like" + "'" + origem + "' and path.destino not in ('"+ visitados.join("','") +"')" );
       let path = [];
       if (result != null) {
         result.rows.forEach(temp => {
           path.push(new Path(temp.origem, temp.distancia, temp.destino));
         })
-        return path;
       }
+      client.release();
+      return path;
     } catch (err){
       console.log(err);
     }
@@ -50,14 +28,15 @@ module.exports = {
 
   calcularCaminho: async function (origem, destino) {
     let distanciaTotal = 0;
-    let conexoes = [];
+    let conexoes;
     let final = '';
     let result = [];
     let pathTemp;
     let distTemp = Infinity;
-    let visitados = [];
-    conexoes = await this.buscarConexoes(origem, null);
+    let visitados = [origem];
+    conexoes = await this.buscarConexoes(origem, visitados);
     while (final != destino) {
+      console.log('entrou while')
       pathTemp = conexoes[0];
       conexoes.forEach(p => {
         if (distTemp > (p.distanciaPath)) {
@@ -74,6 +53,7 @@ module.exports = {
     console.log(`result:  ${result}`);
     console.log(`distancia total: ${distanciaTotal}`)
     return new Resultado(result,distanciaTotal);
+    pool.end();
   },
 
 }
